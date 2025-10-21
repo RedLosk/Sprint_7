ea98d197f9eae682daace5c558f5a12c15e139a1
Lika Nartova <lika.nartova.00@gmail.com>
2025-10-21 21:40:16 +0300

    Added corrections to the project

--- conftest.py
+++ conftest.py
@@ -25,16 +25,3 @@ def courier_cleanup():
         courier_id = login_response.json()["id"]
         delete_url = endpoints.COURIER_DELETE.format(id = courier_id)
         requests.delete(delete_url)
-
-@pytest.fixture()
-def order_data():
-    return {
-    "firstName": "Lika",
-    "lastName": "Nartova",
-    "address": "Цветочная, 619",
-    "metroStation": 2,
-    "phone": "+7 918 00 52 449",
-    "rentTime": 5,
-    "deliveryDate": "2025-10-10",
-    "comment": "Тестовый комментарий"
-}
--- tests/test_create_order.py
+++ tests/test_create_order.py
@@ -2,13 +2,14 @@ import pytest
 import allure
 import requests
 import endpoints
+from helpers import order_data
 
 class TestCreationOrder:
     @allure.title('Cоздания заказа с разными цветами')
     @pytest.mark.parametrize("color_option", [["BLACK"], ["GREY"], ["BLACK", "GREY"], []])
-    def test_creation_order_using_color(self, order_data, color_option):
+    def test_creation_order_using_color(self, color_option):
 
-        order_data_with_color = order_data.copy()
+        order_data_with_color = order_data()
         order_data_with_color["color"] = color_option
 
         with allure.step('Создание заказа'):
